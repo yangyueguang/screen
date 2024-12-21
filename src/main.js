339990e734570faba7719f0a367d997ac6ce3bfd
yangyueguang <2829969299@qq.com>
2024-12-21 16:55:02 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@ import App from './App'
 import router from './router'
 Vue.config.productionTip = false
 new Vue({router, render: h => h(App)}).$mount('#app')
+let Point = function(x, y, z, r, c) {
+  this.x = x || 0
+  this.y = y || 0
+  this.z = z || 0
+  this.a = 1
+  this.color = c || ''
+  this.radius = r || 0
+  this.vx = 0
+  this.vy = 0
+}
+let color = function(a = 'ff') {
+  let r = Math.floor(Math.random() * 256).toString(16).padStart(2, '0')
+  let g = Math.floor(Math.random() * 256).toString(16).padStart(2, '0')
+  let b = Math.floor(Math.random() * 256).toString(16).padStart(2, '0')
+  return `#${r}${g}${b}${a}`
+}
